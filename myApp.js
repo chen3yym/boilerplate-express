3265b8933e51ae3ab48f904077170f43ea6c6ec4
mint_choc <40581631+chen3yym@users.noreply.github.com>
2022-07-04 02:11:18 +0000

    add author to package.json

--- myApp.js
+++ myApp.js
@@ -1,6 +1,16 @@
-let express = require('Hello World');
+console.log('Hello World');
+
+let express = require('express');
 let app = express();
 
+let path = '/';
+
+app.get(path, function(req, res){
+  // res.send('Hello Express');
+  res.sendFile(__dirname + '/views/index.html');
+
+  
+});
 
 
 
